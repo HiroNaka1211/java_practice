1b4403e3fa89cc5d9ae6ac82ffb7889c01a197fa
HiroNaka1211 <N10978607@qut.edu.au>
2023-04-19 23:12:47 +0900

    second commit

---
+++ practice.java
@@ -0,0 +1,105 @@
+
+class Practice {
+    public static void main(String arg[]){
+        // System.out.println("Good morning");
+        // System.out.println("Good afternoon");
+        // System.out.println("Good evening");
+
+        // int var;
+
+        // var = 1;
+
+        // System.out.println(var);
+
+        // int var_a = 10;
+        // int var_b = 1;
+        // boolean var_bool;
+
+        // var_bool = (var_a < var_b);
+
+        // System.out.println(var_bool);
+
+        // String[] arr;
+        // arr = new String[3];
+
+        // arr[0]="sato";
+        // arr[1]="suzuki";
+        // arr[2]="takahashi";
+
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+
+        // String[] arr = {"sato","suzuki","takahashi"};
+        // for(int i = 0; i < arr.length; i++){
+        //     System.out.println(arr[i]);
+        // }
+
+        // String[][] arr;
+        // arr = new String[2][2];
+
+        // arr[0][0] = "sato";
+        // arr[0][1] = "suzuki";
+        // arr[1][0] = "takahashi";
+        // arr[1][1] = "tanaka";
+
+        // System.out.println(arr[0][0]);
+        // System.out.println(arr[0][1]);
+        // System.out.println(arr[1][0]);
+        // System.out.println(arr[0][1]);
+
+        // String[][] arr = {{"sato","suzuki"},{"takahashi","tanaka"}};
+        // System.out.println(arr[0][0]);
+        // System.out.println(arr[0][1]);
+        // System.out.println(arr[1][0]);
+        // System.out.println(arr[0][1]);
+
+        // int x = 8;
+        // int y = 3;
+
+        // System.out.println(x >= 5 && x <= 10);
+        // System.out.println(y >= 5 && y <= 10);
+        // System.out.println(x == 3 || y == 3);
+        // System.out.println(x == 1 || y == 1);
+
+        // int x = 8;
+        // int y = 12;
+        // int z = 20;
+
+        // System.out.println(x += 10);
+        // System.out.println(z += y);
+
+        // int age = 0;
+
+        // if(age >= 20){
+        //     System.out.println("adult");
+        // } else if (age == 0){
+        //     System.out.println("baby");
+        // } else {
+        //     System.out.println("child");
+        // }
+
+        // for(int i = 0; i <= 4; i++) {
+        //     System.out.println(i);
+        // }
+
+        // for(int i = 0; i <= 4; i++){
+        //     if(i ==3){
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+        // for(int i = 0; i <= 4; i++){
+        //     if(i == 3){
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        for(int i = 0; i <= 2; i++){
+            for(int j = 0; j <= 2; j++){
+                System.out.println((i + "-" + j));
+            }
+        }
+    }
+}
